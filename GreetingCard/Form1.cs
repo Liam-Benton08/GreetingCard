6e276a9dbeb8c09e4e3a2b29a623aebaf933bc3b
Liam-Benton08 <liambent383@SDS-L-2BHDF63>
2024-04-22 14:55:12 -0400

    April 22, 2024. School.

--- GreetingCard/Form1.cs
+++ GreetingCard/Form1.cs
@@ -24,9 +24,18 @@ namespace GreetingCard
         int heartp4 = 140;
         int heartp5 = 200;
         int time = 150;
+
+        Graphics g;
+        Graphics g2;
+        Bitmap bmp; 
+
         public Form1()
         {
             InitializeComponent();
+            g2 = this.CreateGraphics();  
+            bmp = new Bitmap(this.Width, this.Height);
+            g = Graphics.FromImage(bmp);    
+
         }
 
         private void Form1_Shown(object sender, EventArgs e)
@@ -58,26 +67,15 @@ namespace GreetingCard
             g.DrawLine(redPen, 420, 140, 373, 200);
             g.FillEllipse(redBrush, 325, 100, 50, 50);
             g.FillEllipse(redBrush, 375, 100, 50, 50);
-            
 
+            g2.DrawImage(bmp, 0, 0);
         }
 
         private void Form1_Click(object sender, EventArgs e)
         {
-            int frontWord = 340;
-            int backWord = 700;
-            int balloon = 50;
-            int line1 = 165;
-            int line2 = 300;
-            int heartp1 = 125;
-            int heartp2 = 163;
-            int heartp3 = 100;
-            int heartp4 = 140;
-            int heartp5 = 200;
-            int arrow = 0;
             int time = 150;
 
-            Graphics g = this.CreateGraphics();
+           // Graphics g = this.CreateGraphics();
 
             g.Clear(Color.Purple);
 
@@ -95,6 +93,16 @@ namespace GreetingCard
             Font drawFont = new Font("Arial", 30, FontStyle.Bold);
             Font bigFont = new Font("Arial", 60, FontStyle.Bold);
 
+            int frontWord = 340;
+            int balloon = 50;
+            int line1 = 165;
+            int line2 = 300;
+            int heartp1 = 125;
+            int heartp2 = 163;
+            int heartp3 = 100;
+            int heartp4 = 140;
+            int heartp5 = 200;
+
             while (time >= 0)
             {
                 g.DrawString("Happy Valentines Day!", drawFont, redBrush, 152, frontWord);
@@ -111,8 +119,9 @@ namespace GreetingCard
                 g.DrawLine(pinkPen, 420, heartp4, 373, heartp5);
                 g.FillEllipse(pinkBrush, 325, heartp3, 50, 50);
                 g.FillEllipse(pinkBrush, 375, heartp3, 50, 50);
-
-                Thread.Sleep(07);
+                
+                g2.DrawImage(bmp, 0, 0);
+                Thread.Sleep(02);
                 g.Clear(Color.Purple);
 
                 frontWord = frontWord + 2;
@@ -128,12 +137,14 @@ namespace GreetingCard
                 time--;
             }
 
-            time = 330;
+            time = 350;
+            int backWord = 700;
 
             while (time >= 0)
             {
                 g.DrawString("I had to ask cupid for a map...           Because I just keep getting lost in your eyes.", bigFont, whiteBrush, backWord, 160);
 
+                g2.DrawImage(bmp, 0, 0);
                 Thread.Sleep(10);
                 g.Clear(Color.Red);
 
@@ -142,34 +153,76 @@ namespace GreetingCard
                 time--;
             }
 
-            time = 40;
+            time = 10;
+
+            int arrowp1 = 290;
+            int arrowp2 = 250;
+            int arrowp3 = 300;
+            int arrowp4 = 160;
+            int arrowp5 = 155;
 
             while (time >= 0)
             {
                 g.Clear(Color.Pink);
-                g.DrawPie(blackPen2, 290, 191, 60, 60, 150, 70);
-                g.FillPie(whiteBrush, 290, 191, 60, 60, 150, 70);
-                g.FillRectangle(pinkBrush, 250, 200, 50, 50);
+                g.DrawPie(blackPen2, arrowp1, 191, 60, 60, 150, 70);
+                g.FillPie(whiteBrush, arrowp1, 191, 60, 60, 150, 70);
+                g.FillRectangle(pinkBrush, arrowp2, 200, 50, 50);
                 g.DrawEllipse(brownPen, 25, 78, 240, 300);
                 g.FillRectangle(pinkBrush, 0, 0, 150, 500);
                 g.DrawLine(whitePen, 157, 83, 157, 375);
-                g.DrawLine(blackPen, 300, 201, 300, 236);
+                g.DrawLine(blackPen, arrowp3, 201, arrowp3, 236);
                 g.DrawEllipse(blackPen2, 500, 155, 130, 130);
                 g.FillEllipse(yellowBrush, 500, 155, 130, 130);
                 g.FillEllipse(blackBrush, 520, 185, 35, 35);
                 g.FillEllipse(blackBrush, 575, 185, 35, 35);
                 g.DrawArc(blackPen2, 520, 210, 100, 50, 50, 100);
-                g.DrawLine(blackPen, 160, 220, 300, 220);
-                g.DrawLine(blackPen, 160, 220, 155, 215);
-                g.DrawLine(blackPen, 160, 220, 155, 225);
-                
+                g.DrawLine(blackPen, arrowp4, 220, arrowp3, 220);
+                g.DrawLine(blackPen, arrowp4, 220, arrowp5, 215);
+                g.DrawLine(blackPen, arrowp4, 220, arrowp5, 225);
 
+                arrowp1 = arrowp1 + 20;
+                arrowp2 = arrowp2 + 20;
+                arrowp3 = arrowp3 + 20;
+                arrowp4 = arrowp4 + 20;
+                arrowp5 = arrowp5 + 20;
 
                 time--;
+
+                g2.DrawImage(bmp, 0, 0);
+                Thread.Sleep(20);
             }
+
+                g.Clear(Color.Tomato);
+
+                g.DrawEllipse(blackPen2, 500, 155, 130, 130);
+                g.FillEllipse(yellowBrush, 500, 155, 130, 130);
+                g.FillEllipse(blackBrush, 520, 185, 35, 35);
+                g.FillEllipse(blackBrush, 575, 185, 35, 35);
+                g.DrawArc(blackPen2, 520, 210, 100, 50, 50, 100);
+
+                g.FillEllipse(redBrush, 512, 183, 25, 25);
+                g.FillEllipse(redBrush, 537, 183, 25, 25);
+                g.DrawEllipse(redPen, 512, 183, 25, 25);
+                g.DrawEllipse(redPen, 537, 183, 25,25);
+                g.DrawLine(redPen, 512, 200, 539, 230);
+                g.DrawLine(redPen, 562, 200, 535, 230);
+                g.FillEllipse(redBrush, 527, 200, 20, 20);
+                g.FillEllipse(redBrush, 527, 205, 20, 20);
+
+                g.FillEllipse(redBrush, 567, 183, 25, 25);
+                g.FillEllipse(redBrush, 592, 183, 25, 25);
+                g.DrawEllipse(redPen, 567, 183, 25, 25);
+                g.DrawEllipse(redPen, 592, 183, 25, 25);
+                g.DrawLine(redPen, 567, 200, 594, 230);
+                g.DrawLine(redPen, 617, 200, 590, 230);
+                g.FillEllipse(redBrush, 582, 200, 20, 20);
+                g.FillEllipse(redBrush, 582, 205, 20, 20);
             
+                g.DrawString("Wowza", bigFont, blackBrush, 150, 170);
 
+                g2.DrawImage(bmp, 0, 0);
 
+            //Add comments
         }
     }
 }
